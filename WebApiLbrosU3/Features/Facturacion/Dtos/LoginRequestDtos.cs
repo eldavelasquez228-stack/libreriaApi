57c9bb9dcd61cd3c9e46a35a35f2e2fb71139825
Elda <eldavelasquez228@gmail.com>
2026-04-15 13:32:03 -0600

    Primer commit

---
+++ WebApiLbrosU3/Features/Facturacion/Dtos/LoginRequestDtos.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiLbrosU3.Commons.Dtos
+{
+    public class LoginRequestDtos
+    {
+        public string Correo { get; set; }
+        public string Password { get; set; } 
+    }
+}
--- WebApiLbrosU3/obj/Debug/net10.0/WebApiLbrosU3.AssemblyInfo.cs
+++ WebApiLbrosU3/obj/Debug/net10.0/WebApiLbrosU3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiLbrosU3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f6276745b84ff5fa94e87a5df8c4138336f53f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiLbrosU3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiLbrosU3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
